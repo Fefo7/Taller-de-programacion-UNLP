e8735442e8bc043f646551f9301020a666f1152d
Federico Jose Isla <federicoisla1@gmail.com>
2024-08-29 22:08:18 -0300

    termine la practica 3 hay un bug al final de la muestra de datos

--- Practica3/main2.pas
+++ Practica3/main2.pas
@@ -1,329 +1,344 @@
-program main2;
+
+Program main2;
 
 
-type
-	Rdia= 1..31;
-	Rmes=1..12;
-	dateTime = record
-		dia: Rdia;
-		mes: Rmes;
-		anio: integer;
-	end;
-	venta = record	
-		code:integer;
-		fecha:dateTime;
-		UnidadesVen: integer;
-	end;
-	
-	arbol = ^nodo;
-	nodo = record
-		elem: venta;
-		HI: arbol;
-		HD:arbol;
-	end;
-	
-	VentaResumen = record
-		code:integer;
-		UnidadesVen: integer;
-	end;
-	
-	arbol2 = ^nodo2;
-	nodo2 = record
-		elem: VentaResumen;
-		HI: arbol2;
-		HD:arbol2;
-	end;
-	
-	listVenta= ^nodoList;
-	nodoList = record
-		elem: venta;
-		sig:listVenta;
-	end;
-	
-	produXlista= record
-		code: integer;
-		ventas: listVenta;
-	end;
-	
-	arbol3 = ^nodo3;
-	nodo3 = record
-		elem: produXlista;
-		HI: arbol3;
-		HD:arbol3;
-	end;
-	
+Type 
+  Rdia = 1..31;
+  Rmes = 1..12;
+  dateTime = Record
+    dia: Rdia;
+    mes: Rmes;
+    anio: integer;
+  End;
+  venta = Record
+    code: integer;
+    fecha: dateTime;
+    UnidadesVen: integer;
+  End;
 
-function GenerateRandomR ( min: integer; max:integer):integer;
-begin
-		GenerateRandomR := Random(max - min + 1) + min;
-end;
+  arbol = ^nodo;
+  nodo = Record
+    elem: venta;
+    HI: arbol;
+    HD: arbol;
+  End;
+
+  VentaResumen = Record
+    code: integer;
+    UnidadesVen: integer;
+  End;
+
+  arbol2 = ^nodo2;
+  nodo2 = Record
+    elem: VentaResumen;
+    HI: arbol2;
+    HD: arbol2;
+  End;
+
+  listVenta = ^nodoList;
+  nodoList = Record
+    elem: venta;
+    sig: listVenta;
+  End;
+
+  produXlista = Record
+    code: integer;
+    ventas: listVenta;
+  End;
+
+  arbol3 = ^nodo3;
+  nodo3 = Record
+    elem: produXlista;
+    HI: arbol3;
+    HD: arbol3;
+  End;
 
 
-function tomarFecha: dateTime;
-var
-	f: dateTime;
-begin
-	
-	f.dia:= GenerateRandomR(1, 31);
-	f.mes:= GenerateRandomR(1, 12);
-	f.anio:= GenerateRandomR(2024, 2030);
-	tomarFecha:= f;
-	
-end;
+Function GenerateRandomR ( min: integer; max:integer): integer;
+Begin
+  GenerateRandomR := Random(max - min + 1) + min;
+End;
+
+
+Function tomarFecha: dateTime;
+
+Var 
+  f: dateTime;
+Begin
+
+  f.dia := GenerateRandomR(1, 31);
+  f.mes := GenerateRandomR(1, 12);
+  f.anio := GenerateRandomR(2024, 2030);
+  tomarFecha := f;
+
+End;
 
 {agregar nodo arbol 1}
 
-procedure InsertarNodo(var a:arbol; v: venta);
-	begin
-		if(a=nil) then
-		begin
-			new(a);
-			a^.elem:= v;
-			a^.HD:= nil;
-			a^.HI:= nil;
-		end
-		else
-			if(v.code < a^.elem.code)then
-				InsertarNodo(a^.HI,v)
-			else
-				InsertarNodo(a^.HD,v);	
-	end;
+Procedure InsertarNodo(Var a:arbol; v: venta);
+Begin
+  If (a=Nil) Then
+    Begin
+      new(a);
+      a^.elem := v;
+      a^.HD := Nil;
+      a^.HI := Nil;
+    End
+  Else
+    If (v.code < a^.elem.code)Then
+      InsertarNodo(a^.HI,v)
+  Else
+    InsertarNodo(a^.HD,v);
+End;
 
 {agregar nodo arbol 2}
-procedure InsertarNodo2(var a:arbol2; code: integer; cantUni: integer);
-	begin
-		if(a=nil) then
-		begin
-			new(a);
-			a^.elem.code:= code;
-			a^.elem.UnidadesVen:= cantUni;
-			a^.HD:= nil;
-			a^.HI:= nil;
-		end
-		else
-			if(code < a^.elem.code)then
-				InsertarNodo2(a^.HI,code, cantUni)
-			else
-				InsertarNodo2(a^.HD,code,cantUni);	
-	end;
+Procedure InsertarNodo2(Var a:arbol2; code: integer; cantUni: integer);
+Begin
+  If (a=Nil) Then
+    Begin
+      new(a);
+      a^.elem.code := code;
+      a^.elem.UnidadesVen := cantUni;
+      a^.HD := Nil;
+      a^.HI := Nil;
+    End
+  Else
+    If (code < a^.elem.code)Then
+      InsertarNodo2(a^.HI,code, cantUni)
+  Else
+    InsertarNodo2(a^.HD,code,cantUni);
+End;
 
 
 
 
 {agregar nodo arbol 3}
 
-procedure InsertarNodo3(var a:arbol3; v:venta);
+Procedure InsertarNodo3(Var a:arbol3; v:venta);
 
-	procedure CargarVentaList(var l:listVenta; v:venta);
-		var
-			aux: listVenta;
-		begin
-			New(aux);
-			aux^.elem := v;
-			aux^.sig := l;
-			l := aux;
-		end;
-		
-begin
-	if(a=nil) then
-	begin
-		new(a);
-		a^.elem.code:= v.code;
-		a^.elem.ventas:= nil;
-		CargarVentaList(a^.elem.ventas, v);
-		a^.HD:= nil;
-		a^.HI:= nil;
-	end
-	else
-		if(v.code = a^.elem.code)then
-		        CargarVentaList(a^.elem.ventas, v)
-		else if (v.code > a^.elem.code) then
-			InsertarNodo3(a^.HI,v)
-		else
-			InsertarNodo3(a^.HD,v);
-end;
+Procedure CargarVentaList(Var l:listVenta; v:venta);
+
+Var 
+  aux: listVenta;
+Begin
+  New(aux);
+  aux^.elem := v;
+  aux^.sig := l;
+  l := aux;
+End;
+
+Begin
+  If (a=Nil) Then
+    Begin
+      new(a);
+      a^.elem.code := v.code;
+      a^.elem.ventas := Nil;
+      CargarVentaList(a^.elem.ventas, v);
+      a^.HD := Nil;
+      a^.HI := Nil;
+    End
+  Else
+    If (v.code = a^.elem.code)Then
+      CargarVentaList(a^.elem.ventas, v)
+  Else If (v.code > a^.elem.code) Then
+         InsertarNodo3(a^.HI,v)
+  Else
+    InsertarNodo3(a^.HD,v);
+End;
 
 {Inciso A}
-procedure tomarVenta(var arbolOrdenado:arbol;var  arbolProducto:arbol2; 
-						var arbolProductoLista: arbol3 );
-var
-	v:venta;
-begin
-	
-	v.code:= GenerateRandomR(0, 50);
-	while(v.code<>0)do
-	begin
-		v.fecha:= tomarFecha();
-		v.UnidadesVen:=  GenerateRandomR(1, 10);
-		InsertarNodo(arbolOrdenado,v);
-		InsertarNodo2(arbolProducto, v.code, v.UnidadesVen );
-		InsertarNodo3(arbolProductoLista, v);
-		v.code:= GenerateRandomR(0, 20);
-	end;
-	
-	
-end;
+Procedure tomarVenta(Var arbolOrdenado:arbol;Var  arbolProducto:arbol2;
+                     Var arbolProductoLista: arbol3 );
+
+Var 
+  v: venta;
+Begin
+
+  v.code := GenerateRandomR(0, 50);
+  While (v.code<>0) Do
+    Begin
+      v.fecha := tomarFecha();
+      v.UnidadesVen :=  GenerateRandomR(1, 10);
+      InsertarNodo(arbolOrdenado,v);
+      InsertarNodo2(arbolProducto, v.code, v.UnidadesVen );
+      InsertarNodo3(arbolProductoLista, v);
+      v.code := GenerateRandomR(0, 20);
+    End;
+
+
+End;
 
 {Inciso B }
 Function BuscarElemento(a:arbol; f:dateTime): integer ;
-	
-	function EsIgual (f1:dateTime; f2:dateTime) : boolean;
-		begin
-			EsIgual:= (f1.dia = f2.dia) and (f1.mes = f2.mes) and (f1.anio = f2.anio) ;							
-		end;
-		
-begin
-	if(a<>nil)then BuscarElemento:= 0
-	else
-		if EsIgual(a^.elem.fecha, f)  then
-			BuscarElemento:= a^.elem.UnidadesVen + BuscarElemento(a^.HI, f) +BuscarElemento(a^.HD, f)
-		else
-			BuscarElemento := BuscarElemento(a^.HI, f) + BuscarElemento(a^.HD, f);
-			
-end;
+
+Function EsIgual (f1:dateTime; f2:dateTime) : boolean;
+Begin
+  EsIgual := (f1.dia = f2.dia) And (f1.mes = f2.mes) And (f1.anio = f2.anio) ;
+End;
+
+Begin
+  If (a<>Nil)Then BuscarElemento := 0
+  Else
+    If EsIgual(a^.elem.fecha, f)  Then
+      BuscarElemento := a^.elem.UnidadesVen + BuscarElemento(a^.HI, f) +
+                        BuscarElemento(a^.HD, f)
+  Else
+    BuscarElemento := BuscarElemento(a^.HI, f) + BuscarElemento(a^.HD, f);
+
+End;
 
 {Inciso B Usuario}
-procedure ProductosMasVendidos(a:arbol);
-var
-f: dateTime;
-begin
-	writeln('ingrese el dia');
-	readln(f.dia);
-	writeln('ingrese el mes');
-	readln(f.mes);
-	writeln('ingrese el anio');
-	readln(f.anio);
-	writeln('cantidades: ', BuscarElemento(a,f));
-end;
+Procedure ProductosMasVendidos(a:arbol);
+
+Var 
+  f: dateTime;
+Begin
+  writeln('ingrese el dia');
+  readln(f.dia);
+  writeln('ingrese el mes');
+  readln(f.mes);
+  writeln('ingrese el anio');
+  readln(f.anio);
+  writeln('cantidades: ', BuscarElemento(a,f));
+End;
 
 {Inciso C }
 
-procedure MaximoVendido (a: arbol2;var  maxU: integer;var codeMax:integer);
-	
-	procedure actualizarMaximo (code: integer; uniVendi:integer;var  max:integer; var codeMax:integer );
-	begin
-		if (uniVendi > max) then
-		begin
-			max:= uniVendi;
-			codeMax:= code;
-		end;
-	
-	end;
+Procedure MaximoVendido (a: arbol2;Var  maxU: integer;Var codeMax:integer);
 
-		
-begin
-	if(a<> nil)then
-	begin
-		actualizarMaximo(a^.elem.code, a^.elem.UnidadesVen ,maxU, codeMax);
-		MaximoVendido(a^.HI, maxU, codeMax);
-		MaximoVendido(a^.HD, maxU, codeMax);
-	end;
-	
-end;
+Procedure actualizarMaximo (code: integer; uniVendi:integer;Var  max:integer;
+                            Var codeMax:integer );
+Begin
+  If (uniVendi > max) Then
+    Begin
+      max := uniVendi;
+      codeMax := code;
+    End;
+
+End;
+
+
+Begin
+  If (a<> Nil)Then
+    Begin
+      actualizarMaximo(a^.elem.code, a^.elem.UnidadesVen ,maxU, codeMax);
+      MaximoVendido(a^.HI, maxU, codeMax);
+      MaximoVendido(a^.HD, maxU, codeMax);
+    End;
+
+End;
 
 
 {Inciso D}
-procedure MaximaCantVentas(a: arbol3;var code: integer;var MaxV:Integer);
-	
-	function ContarVentas(l:listVenta):integer;
-	var
-		cant: integer;
-	begin
-		cant:= 0;
-		while (l<> nil) do
-		begin
-			cant:=  +1;
-			l:=l^.sig;
-		end;
-		ContarVentas:= cant;
-	end;
-	
-	procedure buscoMaximo (cantV: integer;code:integer ;var codeMax:integer; var MaxV: integer);
-	begin
-		if(cantV > MaxV)then
-		begin
-			MaxV:= cantV;
-			codeMax:= code;
-		end;
-	end;
-	
-begin
-	if(a <> nil) then
-	begin
-		buscoMaximo(ContarVentas(a^.elem.ventas), a^.elem.code, code, MaxV );
-		MaximaCantVentas(a^.HI, code, MaxV);
-		MaximaCantVentas(a^.HD, code, MaxV);
-	end;
-end;
+Procedure MaximaCantVentas(a: arbol3;Var code: integer;Var MaxV:Integer);
+
+Function ContarVentas(l:listVenta): integer;
+
+Var 
+  cant: integer;
+Begin
+  cant := 0;
+  While (l<> Nil) Do
+    Begin
+      cant :=  +1;
+      l := l^.sig;
+    End;
+  ContarVentas := cant;
+End;
+
+Procedure buscoMaximo (cantV: integer;code:integer ;Var codeMax:integer; Var
+                       MaxV: integer);
+Begin
+  If (cantV > MaxV)Then
+    Begin
+      MaxV := cantV;
+      codeMax := code;
+    End;
+End;
+
+Begin
+  If (a <> Nil) Then
+    Begin
+      buscoMaximo(ContarVentas(a^.elem.ventas), a^.elem.code, code, MaxV );
+      MaximaCantVentas(a^.HI, code, MaxV);
+      MaximaCantVentas(a^.HD, code, MaxV);
+    End;
+End;
 
 
 
 {Procesos para mostrar los arboles para comparar los resultados}
 
-procedure MostrarArbol1(a:arbol);
-begin
-	if(a <> nil)then
-	begin
-		writeln('funciono? ', a^.elem.fecha.dia);
-		writeln('funciono? ', a^.elem.fecha.mes);
-		writeln('funciono? ', a^.elem.fecha.anio);
-		writeln;
-		MostrarArbol1(a^.HI);
-		MostrarArbol1(a^.HD);
-	
-	end;
-end;
+Procedure MostrarArbol1(a:arbol);
+Begin
+  If (a <> Nil)Then
+    Begin
+      writeln('funciono? ', a^.elem.fecha.dia);
+      writeln('funciono? ', a^.elem.fecha.mes);
+      writeln('funciono? ', a^.elem.fecha.anio);
+      writeln;
+      MostrarArbol1(a^.HI);
+      MostrarArbol1(a^.HD);
+
+    End;
+End;
 
 
-procedure MostrarArbol2(a:arbol2);
-begin
-	if(a <> nil)then
-	begin
-		writeln('Code: ', a^.elem.code);
-		writeln('Unidades: ', a^.elem.UnidadesVen);
-		writeln('-----------------------------------------------------------------');
-		MostrarArbol2(a^.HI);
-		MostrarArbol2(a^.HD);
-	
-	end;
-end;
+Procedure MostrarArbol2(a:arbol2);
+Begin
+  If (a <> Nil)Then
+    Begin
+      writeln('Code: ', a^.elem.code);
+      writeln('Unidades: ', a^.elem.UnidadesVen);
+      writeln(
 
 
-procedure MostrarArbol3(a:arbol3);
-begin
-	if(a <> nil)then
-	begin
-		writeln('funciono? ', a^.elem.code);
-		MostrarArbol3(a^.HI);
-		MostrarArbol3(a^.HD);
-	
-	end;
-end;
+             '-----------------------------------------------------------------'
+      );
+      MostrarArbol2(a^.HI);
+      MostrarArbol2(a^.HD);
 
-var
-	a:arbol;
-	arbolProducto:arbol2;
-	arbolProductoLista : arbol3;
-	max, codeMax: integer;
-	codeMax2, max2: integer;
-BEGIN
-	a:= nil;
-	arbolProducto:= nil;
-	arbolProductoLista:= nil;
-	max:= -1;
-	max2:= -1;
-	Randomize; 
-	
-	tomarVenta(a,arbolProducto,arbolProductoLista);
-	{MostrarArbol1(a);
+    End;
+End;
+
+
+Procedure MostrarArbol3(a:arbol3);
+Begin
+  If (a <> Nil)Then
+    Begin
+      writeln('funciono? ', a^.elem.code);
+      MostrarArbol3(a^.HI);
+      MostrarArbol3(a^.HD);
+
+    End;
+End;
+
+Var 
+  a: arbol;
+  arbolProducto: arbol2;
+  arbolProductoLista : arbol3;
+  max, codeMax: integer;
+  codeMax2, max2: integer;
+Begin
+  a := Nil;
+  arbolProducto := Nil;
+  arbolProductoLista := Nil;
+  max := -1;
+  max2 := -1;
+  Randomize;
+
+  tomarVenta(a,arbolProducto,arbolProductoLista);
+
+
+
+{MostrarArbol1(a);
 	writeln;
 	MostrarArbol2(arbolProducto);
 	writeln;
 	MostrarArbol3(arbolProductoLista);}
-	ProductosMasVendidos(a);
-	MaximoVendido(arbolProducto, max, codeMax);
-	writeln('Codigo maximas unidades vendidas: ',codeMax);
-	MaximaCantVentas(arbolProductoLista,codeMax2, max2);
-	writeln('Codigo del producto con mas ventas: ',codeMax2);
-END.
-
+  ProductosMasVendidos(a);
+  MaximoVendido(arbolProducto, max, codeMax);
+  writeln('Codigo maximas unidades vendidas: ',codeMax);
+  MaximaCantVentas(arbolProductoLista,codeMax2, max2);
+  writeln('Codigo del producto con mas ventas: ',codeMax2);
+End.
---
+++ Practica3/main3.pas
@@ -0,0 +1,192 @@
+program main3;
+
+type
+	Rnota= 1..10;
+	Examen = record
+		legajo:integer;
+		codeMateria: integer;
+		fecha:string;
+		nota: Rnota;
+	end;
+	
+	listExamen= ^nodoL;
+	nodoL = record
+		elem: Examen;
+		sig:listExamen;
+	end;
+	{prodria poner el legajo en el registro de alumno?} {ExamenF: Examen;}
+	alumno = record
+		legajo:integer; {podria no estarlo y dejar solo el legajo aca}
+		ListaExamenes: listExamen;
+	end;
+	
+	arbol= ^nodoA;
+	nodoA = record
+		elem: alumno;
+		hi:arbol; 
+		hd:arbol;
+		end;
+	
+procedure leerExamen(var e:examen);
+begin
+	writeln('ingrese el legajo: ');
+	readln(e.legajo);
+	if(e.legajo <> 0) then
+	begin
+		writeln('ingrese el codigo de la materia: ');
+		readln(e.codeMateria);
+		writeln('ingrese la fecha del examen: ');
+		readln(e.fecha);
+		writeln('ingrese la nota del examen: ');
+		readln(e.nota);
+	end;
+end;	
+
+procedure CargarLista(var l:listExamen ; e:examen);
+var 
+	aux: listExamen;
+begin
+	new(aux);
+	aux^.elem:= e;
+	aux^.sig:= l;
+	l:= aux;
+end;
+
+
+procedure InsertarNodo(var a: arbol; e:Examen);
+var
+	aux: arbol;
+begin
+	if(a= nil) then
+	begin
+		new(aux);
+		aux^.elem.legajo:= e.legajo;
+		aux^.elem.ListaExamenes := nil;
+		CargarLista(aux^.elem.ListaExamenes, e);
+		aux^.hi:= nil;
+		aux^.hd:= nil;
+		a:= aux;
+	end
+	else
+		if (a^.elem.legajo = e.legajo) then
+			CargarLista(a^.elem.ListaExamenes, e)
+		else
+			begin
+			if (a^.elem.legajo > e.legajo) then
+				InsertarNodo(a^.hi,e)
+			else
+				InsertarNodo(a^.hd, e);
+			end;
+end;
+			
+procedure CargarExamen (var a:arbol);
+var
+	e:Examen;
+begin
+	leerExamen(e);
+	while(e.legajo <> 0)do
+	begin
+		InsertarNodo(a,e);
+		leerExamen(e);
+	end;
+end;
+
+function IsImpar( legajo: integer) : boolean;
+begin
+	IsImpar:= (legajo mod 2) <>  0;
+end;
+
+procedure  cantAlumnosImpar (a: arbol; var cant: integer);
+begin
+	if (a<> nil) then
+	begin
+		if(IsImpar(a^.elem.legajo)) then cant:= cant+1;
+		cantAlumnosImpar(a^.hi, cant);
+		cantAlumnosImpar(a^.hd,cant);
+	end;
+
+end;
+
+procedure MostrarArbol(a:arbol);
+begin
+	if(a<> nil) then
+	begin
+		writeln('numero de legajo', a^.elem.legajo);
+		MostrarArbol(a^.hi);
+		MostrarArbol(a^.hd);
+	end;
+end;
+
+procedure calcularAprobados (l: listExamen; var aprob:integer);
+begin
+	while (l <> nil) do
+	begin
+		if(l^.elem.nota >=  4)then aprob:= aprob +1;
+		l:= l^.sig;
+	end;
+end;
+
+
+procedure MostrarFAprobados(a:arbol);
+var
+	aprob: integer;
+begin
+	if(a<>nil) then
+	begin
+		aprob:= 0;
+		calcularAprobados(a^.elem.ListaExamenes,aprob);
+		writeln(a^.elem.legajo, ' aprobo: ', aprob);
+		MostrarFAprobados(a^.hi);
+		MostrarFAprobados(a^.hd);
+	end;
+end;
+
+{Un módulo que reciba la estructura generada en a. y un valor real. Este módulo debe
+retornar los legajos y promedios de los alumnos cuyo promedio supera el valor ingresado.}
+procedure calcularPromedio (l: listExamen;var promedio : real);
+var
+	cantNotas: integer;
+begin
+	promedio:= 0;
+	cantNotas:= 0;
+	while (l<> nil) do
+	begin
+		cantNotas:= cantNotas +1;
+		promedio:= promedio + l^.elem.nota;
+		l:= l^.sig;
+	end;
+	promedio:= promedio / cantNotas;
+end;
+procedure BuscarPromedios (a:arbol; numero:real);
+var
+	promedio:reaL;
+begin
+	if (a<>nil) then
+	begin
+		calcularPromedio(a^.elem.ListaExamenes, promedio);
+		if(promedio > numero ) then
+			writeln(a^.elem.legajo, ' promedio del alumno: ', promedio :2:0 );
+		MostrarFAprobados(a^.hi);
+		MostrarFAprobados(a^.hd);
+	end;
+end;
+	
+var 
+	a:arbol;
+	cantAlum:integer;
+	tope: real;
+BEGIN
+	cantAlum:= 0;
+	CargarExamen(a);
+	MostrarArbol(a);
+	cantAlumnosImpar(a,cantAlum);
+	writeln('cantidad de alumnos con legajo impar: ', cantAlum);
+	
+	MostrarFAprobados(a);
+	writeln;
+	
+	writeln('ingrese el tope del promedio');
+	readln(tope);
+	BuscarPromedios(a, tope);
+END.
+
